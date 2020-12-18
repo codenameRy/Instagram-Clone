9b971858a663eb819994f8170506fbac23660442
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-18 01:33:35 -0500

    Initial Header update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import "./App.css";
+import Instagram  from './images/instagram_header.png';
 
 function App() {
   return (
     <div className="App">
-      Hello you clever programmers! Lets build an Instagram Clone with ReactJS 🚀 
+      <h1>Hello you clever programmers! Lets build an Instagram Clone with ReactJS 🚀 </h1>
+
+      <div className="app__header">
+      <img src = {Instagram}
+      alt="instagram"
+
+      />
+      </div>
+
+
+
+      {/* IG Header */}
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
