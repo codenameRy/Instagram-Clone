8b150522798e0b48c18b15fdb6846079fdd15953
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-20 23:34:09 -0500

    Post Avatar update

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,12 +1,17 @@
 //Component to design the post visual
 import React from "react";
-import ReactImage from "../images/react-image-1.jpg";
 import './Post.css';
+import ReactImage from "../images/react-image-1.jpg";
+import RyImage from '../images/RPic2.jpg'
+import Avatar from '@material-ui/core/Avatar'
 
 function Post() {
   return (
     <div className="post">
+      <div className="post__header">
+      <Avatar className="post__avatar" alt="RLee" src={RyImage} />
       <h3>Username</h3>
+      </div>
       {/* Header -> Avatar + Username */}
 
       <img className="post__image" src={ReactImage} alt="react" />
