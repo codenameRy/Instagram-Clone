b987e1709f15663f10dac563144b2495b9790703
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-21 19:38:58 -0500

    Passing props through Post component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./App.css";
 import InstagramHeader  from './images/instagram_header.png';
+import ReactImage from './images/react-image-1.jpg'
 import Post from './Post/Post';
 
 function App() {
@@ -18,6 +19,8 @@ function App() {
 
       <h1>Hello you clever programmers! Lets build an Instagram Clone with ReactJS 🚀 </h1>
       
+      <Post username = "theMagicDon" caption="Wow it works" imageURL="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"/>
+      <Post />
       <Post />
       {/* Posts */}
       {/* Posts */}
