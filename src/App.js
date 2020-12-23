380033fc8550d9edab44dbc785de2bd3d2d79325
Ryan Lee <ryanm.lee11@gmail.com>
2020-12-23 17:57:58 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -36,9 +36,9 @@ function App() {
         ))
       }
       
-      <Post username="theMagicDon" caption="Wow it works" imageURL="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"/>
+      {/* <Post username="theMagicDon" caption="Wow it works" imageURL="https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"/>
       <Post username="peace" caption="tranquil vibes" imageURL="https://www.freecodecamp.org/news/content/images/size/w2000/2020/10/461803-angular-JavaScript-HTML-1.jpg"/>
-      <Post username="fortunetella" caption="Future" />
+      <Post username="fortunetella" caption="Future" /> */}
       {/* Posts */}
       {/* Posts */}
     </div>
