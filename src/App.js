7c3cced23a6c9f9f86c521a2fb7d4449f12d2813
Ryan Lee <ryanm.lee11@gmail.com>
2021-01-26 22:04:07 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
 
       {
         posts.map(({id, post}) => (
-          //The addition of key id from post will allow the app to only render the new unique post on the react app
+          //The addition of key id from post will allow the app to only render the new unique post on the react apps
           <Post key={id} username={post.username} caption={post.caption} imageURL={post.imageURL}/>
         ))
       }
